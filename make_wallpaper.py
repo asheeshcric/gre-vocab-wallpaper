debc1fbea20ad7dad703d0e77b7c4af0ec624d0e
asheeshcric <ashiz2013@gmail.com>
2018-05-24 19:53:47 +0545

    [ADD] Track last updated date for today's words in the file

--- make_wallpaper.py
+++ make_wallpaper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import datetime
 import json
 import os
 import random
@@ -21,22 +22,32 @@ def get_random_words(lines):
         count += 1
         if count == 10 or len(lines) - len(open('seen-words.txt').readlines()) < 10:
             break
-    print(words)
     return words
+
+
+def update_words(lines):
+    today_words = {'words': get_random_words(lines)}
+    today_words['last_updated'] = datetime.datetime.today().strftime("%Y-%m-%d")
+    today_words['last_word'] = None
+    with open('today-words.txt', 'w') as f1:
+        f1.write(json.dumps(today_words))
 
 
 def main():
     lines = open('vocab-list.txt').readlines()
 
     if os.stat("today-words.txt").st_size == 0:
-        today_words = get_random_words(lines)
-        with open('today-words.txt', 'a') as f1:
-            f1.write(json.dumps(today_words))
+        # For an empty file
+        update_words(lines)
 
+    if (json.loads(open('today-words.txt').read())).get('last_updated') != datetime.datetime.today().strftime("%Y-%m-%d"):
+        # Update the set of words everyday
+        update_words(lines)
+    
     today_words = json.loads(open('today-words.txt').read())
-    words = set(today_words.keys()) - {today_words.get('last_word')}
+    words = set(today_words.get('words').keys()) - {today_words.get('last_word')}
     current_word = random.sample(words, 1)[0]
-    current_meaning = today_words.get(current_word)
+    current_meaning = today_words['words'].get(current_word)
             
     # Editing the background image
     img = Image.open('sample.jpg')
@@ -48,13 +59,17 @@ def main():
     draw.text(((W - w) / 2 - 150, (H - h) / 2 - 200), current_word, (255, 255, 255), font=font1)
 
     font2 = ImageFont.truetype("Aaargh.ttf", 80)
-    current_meaning = textwrap.wrap(current_meaning, width=40)
+    current_meaning = textwrap.wrap(current_meaning, width=35)
     padding = 0
     for line in current_meaning:
         w, h = draw.textsize(str(line))
         draw.text(((W - w) / 2 - 600, (H - h) / 2 + padding), str(line), (255, 255, 255), font=font2)
         padding += 100
     img.save('wallpaper.jpg')
+
+    today_words['last_word'] = current_word
+    with open('today-words.txt', 'w') as f1:
+        f1.write(json.dumps(today_words))
 
 
 if __name__ == "__main__": main()
